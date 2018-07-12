6b49ffa7b25af0d6fbbdc42dd0c47a993d1304ce
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-12 17:03:22 -0400

    works, needs interval

--- EnterpriseBot/bot.js
+++ EnterpriseBot/bot.js
@@ -4,26 +4,31 @@ var config = require('./config');
 //AUTHENTICATION TO TWITTER
 var T = new Twit(config);
 
-//GET posts to RETWEET
-var date = 2018-01-01;
+//GET cybersecurity posts to RETWEET
 var params = {
-    q: 'cybersecurity since:2018-01-01',
+    q: 'cybersecurity',
+    lang: 'en',
+    result_type: 'recent',
     count: 1,
 }
 
 T.get('search/tweets', params, gotData);
 
 function gotData(err, data, response) {
-        //console.log(data);
-        T.post('statuses/retweet', data, doRetweet);
-            /*function doRetweet(err, data., response) {
-                if (err) {
-                    console.log('something is wrong with callback');
-                } else {
-                    console.log(data);
+        console.log(data.statuses[0].id_str);
+        var retweetId = data.statuses[0].id_str;
 
-            */
-           
-            }
+       T.post('statuses/retweet/:id', {id: retweetId}, function(err, response) {
+        if (response) {
+            console.log('Retweet Success');
         }
+        if (err) {
+            console.log('Error');
+        }
+
+        });
     }
+
+
+
+    //currently file gets a post about cybersecurity and retweets it with no message

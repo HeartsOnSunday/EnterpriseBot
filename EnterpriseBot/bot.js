52124674094f23705c83e4d248665087ed944cc4
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-12 15:54:21 -0400

    get request works

--- EnterpriseBot/bot.js
+++ EnterpriseBot/bot.js
@@ -1,10 +1,29 @@
-console.log("its alive");
-
 var Twit = require('twit');
+var config = require('./config');
 
-var T = new Twit({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
-})
+//AUTHENTICATION TO TWITTER
+var T = new Twit(config);
+
+//GET posts to RETWEET
+var date = 2018-01-01;
+var params = {
+    q: 'cybersecurity since:2018-01-01',
+    count: 1,
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+        //console.log(data);
+        T.post('statuses/retweet', data, doRetweet);
+            /*function doRetweet(err, data., response) {
+                if (err) {
+                    console.log('something is wrong with callback');
+                } else {
+                    console.log(data);
+
+            */
+           
+            }
+        }
+    }
